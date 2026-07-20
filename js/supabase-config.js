be9ef31aa3a2b0bc0f07843569489236c8292026
Cristian Martin Arias Saldaña <142331430+Lacanchaa@users.noreply.github.com>
2026-07-20 02:38:51 -0500

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -3,7 +3,7 @@ const SUPABASE_URL = "https://injsroyfyknhfetiibkl.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY =
   "sb_publishable_dDfNw9wpPb1CWzysP25tpA_jbpKBlsx";
 
-const supabaseClient = window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_PUBLISHABLE_KEY
 );
